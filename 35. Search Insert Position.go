44494c7e364ec64a7abe609e996d3e77161d9c65
wangwensheng <845082868@qq.com>
2019-08-19 17:23:48 +0800

    ss

--- 35. Search Insert Position.go
+++ 35. Search Insert Position.go
@@ -2,10 +2,10 @@ package main
 import(
 	"fmt"
 )
-func main(){
-	arr := []int{1,4,6}
-	fmt.Println(searchInsert(arr,3));
-}
+// func main(){
+// 	arr := []int{1,4,6}
+// 	fmt.Println(searchInsert(arr,3));
+// }
 
 func searchInsert(nums []int, target int) int {
     if len(nums) == 0 {
